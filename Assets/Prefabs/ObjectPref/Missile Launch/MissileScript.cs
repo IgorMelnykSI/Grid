067ef8802b224959e295981b7e570a2cc664f794
IgorMelnykSI <ihor.melnyk@etu.unice.fr>
2023-01-09 17:15:48 +0100

    Minor changes

--- Assets/Prefabs/ObjectPref/Missile Launch/MissileScript.cs
+++ Assets/Prefabs/ObjectPref/Missile Launch/MissileScript.cs
@@ -9,7 +9,7 @@ public class MissileScript : MonoBehaviour
     private Transform target;
 
     private float timer = 0.0f;
-    private float waitTime = 0.5f;
+    private float waitTime = 0.8f;
 
     public GameObject _explosion;
 
@@ -31,12 +31,11 @@ public class MissileScript : MonoBehaviour
             float offsetX = -12.0f;
             float offsetY = 12.0f;
 
-            Debug.Log("Target position: " + target.position.x + " , " + target.position.y);
             transform.position = new Vector3(target.position.x + offsetX, target.position.y + offsetY, 0);
             transform.Rotate(0, 0, -90);
             body.rotation = -90f;
             body.velocity = transform.right * speed;
-            //Destroy(gameObject, 3);
+            
             timer = timer - waitTime - 1f;
         }
     }
